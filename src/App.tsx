ca1c91cc224ceb467730bddfe99c79e9fec1879a
Jando Becker <jando@MacBook-Air-de-Jando.local>
2023-06-15 16:03:01 -0600

    Se usa la peticion de SWR y se aplica un loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import React from 'react';
+import './App.css';
+import useSWR from 'swr';
+import axios from 'axios';
 
 interface University {
-  name: string
-  country: string
-  web_pages: string[]
+  name: string;
+  country: string;
+  web_pages: string[];
 }
 
+const fetcher = (url: string) => axios.get(url).then(response => response.data);
+
 const App: React.FC = () => {
-  const [universities, setUniversities] = useState<University[]>([])
-  useEffect(() => {
-    axios
-      .get('http://universities.hipolabs.com/search?country=Mexico')
-      .then(response => {
-        setUniversities(response.data)
-        return response.data
-      })
-      .catch(error => {
-        console.log('Error fetching universities:', error)
-      })
-  }, [])
+  const { data: universities, error } = useSWR<University[]>(
+    'http://universities.hipolabs.com/search?country=Mexico',
+    fetcher
+  );
+
+  if (error) {
+    console.log('Error fetching universities:', error);
+  }
 
   return (
     <div>
-      <h1>Universidades en Mexico</h1>
-      <ul>
-        {universities.map(university => (
-          <li key={university.name}>
-            <h2>{university.name}</h2>
-            <p>Country: {university.country}</p>
-            <p>
-              Website:{' '}
-              <a href={university.web_pages[0]}>{university.web_pages[0]}</a>
-            </p>
-          </li>
-        ))}
-      </ul>
+      <h1>Universidades en México</h1>
+      {!universities ? (
+        <h3>Loading...</h3>
+      ) : (
+        <ul>
+          {universities.map(university => (
+            <li key={university.name}>
+              <h2>{university.name}</h2>
+              <p>Country: {university.country}</p>
+              <p>
+                Website:{' '}
+                <a href={university.web_pages[0]}>{university.web_pages[0]}</a>
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
